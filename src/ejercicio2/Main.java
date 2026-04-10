5061c65fce8d6b66e010b693d4c94448eebd6f47
rgoncar723 <rgoncar723@g.edcuaand.es>
2026-04-10 17:26:05 +0200

    Optimizacion de código, o eso creo yo

--- src/ejercicio2/Main.java
+++ src/ejercicio2/Main.java
@@ -5,43 +5,45 @@ import java.util.Scanner;
 public class Main {
 	@SuppressWarnings("resource")
 	public void show() {
-		String mensaje;
 		Scanner kb = new Scanner(System.in);
 		NotificacionFactory factory = new NotificacionFactory();
 		INotificacion n;
-	
+
 		int opcion = -1;
 		do {
-			System.out.printf("Escoga una opcion:\n 0.Salir\n 1. Enviar un email.\n 2.Enviar un SMS.\n 3.Enviar un Push.\n");
+			System.out.printf(
+					"Escoga una opcion:\n 0.Salir\n 1.Enviar un email.\n 2.Enviar un SMS.\n 3.Enviar un Push.\n");
 			System.out.println("Opcion: ");
 			opcion = kb.nextInt();
-			switch(opcion) {
+			switch (opcion) {
 			case 0 -> System.out.println("Saliendo...");
 			case 1 -> {
-				kb.nextLine();
+
 				n = factory.crearNotificacion("email");
-				System.out.println("Escriba el mensaje que desea enviar");
-				mensaje = kb.nextLine();
-				n.enviar(mensaje);
+				n.enviar(escribirMensaje(kb));
 			}
 			case 2 -> {
-				kb.nextLine();
+
 				n = factory.crearNotificacion("sms");
-				System.out.println("Escriba el mensaje que desea enviar");
-				mensaje = kb.nextLine();
-				n.enviar(mensaje);
+				n.enviar(escribirMensaje(kb));
 			}
 			case 3 -> {
-				kb.nextLine();
+
 				n = factory.crearNotificacion("push");
-				System.out.println("Escriba el mensaje que desea enviar");
-				mensaje = kb.nextLine();
-				n.enviar(mensaje);
+				n.enviar(escribirMensaje(kb));
 			}
 			default -> System.out.println("Opcion invalida");
 			}
-			
-		}while(opcion != 0);
+
+		} while (opcion != 0);
+	}
+
+	String escribirMensaje(Scanner kb) {
+		String mensaje;
+		kb.nextLine();
+		System.out.println("Escriba el mensaje que desea enviar");
+		mensaje = kb.nextLine();
+		return mensaje;
 	}
 
 	public static void main(String[] args) {
