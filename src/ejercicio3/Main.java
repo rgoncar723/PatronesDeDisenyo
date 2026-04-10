253279056f0be53d1de0fd47d92918bffb4be2f4
rgoncar723 <rgoncar723@g.edcuaand.es>
2026-04-10 17:52:25 +0200

    Correcion de errores

--- src/ejercicio3/Main.java
+++ src/ejercicio3/Main.java
@@ -3,11 +3,11 @@ package ejercicio3;
 public class Main {
 	public void show() {
 		Pedido pb = new Pedido.Builder("Pepe", "Calle Santa Maria").build();
-		Pedido pc = new Pedido.Builder("Diego", "Calle 10").setTelefono(610101101).setEmail("Diego@gmail.com").build();
+		Pedido pc = new Pedido.Builder("Diego", "Calle 10").setTelefono(610101101).setEmail("Diego@gmail.com").setObservaciones("Todo de 10").build();
 		System.out.println("Pedido básico");
 		System.out.println(pb.toString());
 
-		System.out.println("Pedido complejo");
+		System.out.println("Pedido completo");
 		System.out.println(pc.toString());
 
 	}
